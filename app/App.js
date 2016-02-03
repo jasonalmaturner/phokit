8874ea5ae4811234b6b9484b1d5a09b9043812fe
Jason Turner <jasonalmaturner@gmail.com>
2016-02-02 21:52:27 -0700

    done with front. I think

--- app/App.js
+++ app/App.js
@@ -3,8 +3,30 @@ import ReactDOM from 'react-dom';
 import axios from 'axios';
 
 const App = React.createClass({
+  getInitialState() {
+    return {
+      loading: false,
+      message: '',
+    };
+  },
+
+  messageClass(classDisplay) {
+    return classDisplay === this.state.message ? '' : 'not-loading ';
+  },
+
+  updateMessage() {
+    setTimeout(() => {
+      this.setState({
+        message: '',
+      });
+    }, 3000);
+  },
+
   handleSubmit(e) {
     e.preventDefault();
+    this.setState({
+      loading: true,
+    });
     console.log(this.refs.email.value);
     axios({
       method: 'post',
@@ -13,9 +35,19 @@ const App = React.createClass({
         email: this.refs.email.value,
       },
     }).then((res) => {
+      console.log(res);
+      this.setState({
+        loading: false,
+        message: res.data === 'email already registered' ? 'whatever' : 'success',
+      });
       this.refs.email.value = '';
+      this.updateMessage();
     }).catch((err) => {
-
+      this.setState({
+        message: 'error',
+        loading: false,
+      });
+      this.updateMessage();
     });
   },
 
@@ -23,9 +55,15 @@ const App = React.createClass({
     return (
       <div>
         <form onSubmit={this.handleSubmit}>
-          <input ref='email' type="email"/>
-          <input type="submit" value="Sign Up" />
+          <input className='input' ref='email' type="email" placeholder='Enter email address'/>
+          <input className='input-button' type="submit" value="Sign Up" />
         </form>
+        <div className='response'>
+          <img className={this.state.loading || 'not-loading'} src="icons/ripple.gif" />
+          <p className={this.messageClass('success') + 'res success'}>Succesfully registered</p>
+          <p className={this.messageClass('error') + 'res error'}>Something bad happened</p>
+          <p className={this.messageClass('whatever') + 'res whatever'}>Already registered</p>
+        </div>
       </div>
     );
   },
