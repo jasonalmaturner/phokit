f1aa6a1c1dcb9dd254c93ee5ac7d06785fd106f8
Jason Turner <jasonalmaturner@gmail.com>
2016-03-02 00:46:11 -0700

    harvesting email before destroying droplet

--- server-assets/controllers/mainCtrl.js
+++ server-assets/controllers/mainCtrl.js
@@ -13,4 +13,12 @@ export default {
       return res.status(500).json(err);
     });
   },
+
+  getEmails(req, res) {
+    Email.find().exec().then(emails => {
+      return res.json(emails);
+    }).catch(err => {
+      return res.status(500).json(err);
+    });
+  },
 };
